5f4b14c8ef9f4ca8560201dd8112e35847aed5e2
Sonia Gorla <soniagorla@msn.com>
2018-08-19 14:15:39 -0500

    Display books on the search page

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ import { Route } from "react-router-dom";
 import MyBooks from "./MyBooks";
 import SearchBooks from "./SearchBooks.js";
 
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   };
 
+  componentDidMount() {
+    BooksAPI.getAll().then(books =>
+      this.setState({
+        books: books
+      })
+    );
+  }
+
   render() {
+    console.log(this.state.books);
     return (
       <div className="app">
         <Route exact path="/" render={() => <MyBooks />} />
-        <Route path="/search" render={() => <SearchBooks />} />
+        <Route
+          path="/search"
+          render={() => <SearchBooks books={this.state.books} />}
+        />
       </div>
     );
   }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -3,7 +3,18 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class SearchBooks extends Component {
-  render() {
+  state = {
+    query: ""
+  };
+
+  updateQuery = query => {
+    this.setState({
+      query: query.trim()
+    });
+  };
+
+  render(props) {
+    let bookList = this.props.books;
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -19,11 +30,43 @@ class SearchBooks extends Component {
                 However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                 you don't find a specific author or title. Every search is limited by search terms.
               */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={event => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid" />
+          {bookList.map(book => (
+            <div className="book">
+              <div className="book-top">
+                <div
+                  className="book-cover"
+                  style={{
+                    width: 128,
+                    height: 188,
+                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                  }}
+                />
+                <div className="book-shelf-changer">
+                  <select>
+                    <option value="move" disabled>
+                      Move to...
+                    </option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors}</div>
+            </div>
+          ))}
         </div>
       </div>
     );
